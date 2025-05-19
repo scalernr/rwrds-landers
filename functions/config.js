13b59f8473af7e6e392b07d8fd208c3a306b3c78
Wrip Andrunn <wripandrunn@Wrips-MacBook-Pro.local>
2025-05-19 17:07:34 -0500

    Add playful-ios config with tracking

--- functions/config.js
+++ functions/config.js
@@ -15,7 +15,18 @@ export async function onRequest(context) {
       useExitTracking: false
     });
   }
-
+  if (referer.includes('/rl/lp1/appv2/tt/fluent/us/playful-ios/')) {
+    return Response.json({
+      logoPath: "/public/assets/prewardslogo3.png",
+      headline: "Test apps and get paid!",
+      ctaText: "Download Here",
+      brandColor: "#b6b1ef", // Playful purple
+      affiliateLink: "https://affrkr.com/?yte=h41EnWbqKhajken6sj5ZWUUYIr110aduvQJDRoz7h5U%3d&s1=",
+      useExitTracking: true,
+      exit_u: "br",
+      exit_p: "IOSFUN"
+    });
+  }
   return Response.json({
     logoPath: "/public/assets/default.png",
     headline: "Claim Your Reward",
