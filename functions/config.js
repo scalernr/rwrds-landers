3b000204dcfbc876424b92e65665a2abeba1fcf3
Wrip Andrunn <wripandrunn@Wrips-MacBook-Pro.local>
2025-05-19 16:14:59 -0500

    Match deployed path in config.js

--- functions/config.js
+++ functions/config.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
   const { pathname } = new URL(context.request.url);
 
   // US Blank (Universal Template)
-  if (pathname.includes('/lp1/appv2/tt/fluent/us/blank/')) {
+  if (pathname.includes('/rl/lp1/appv2/tt/fluent/us/blank/')) {
     return Response.json({
       logoPath: "/public/assets/prewardslogo3.png",
       headline: "Earn Rewards Using Your Phone",
